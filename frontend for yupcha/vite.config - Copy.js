42fe8d057e4d089c6022b9f0596ec59021727a4a
shubhamsinha10-04 <shubhamsinha3412@gmail.com>
2025-07-03 20:27:23 +0530

    Add files via upload

---
+++ frontend for yupcha/vite.config - Copy.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
+
+
